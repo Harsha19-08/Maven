b36c80577dc07474840f5ac785236ba3615df0e7
Harsha199 <Optimist.Coder19@gmail.com>
2024-12-10 19:04:38 +0530

    Maven-Java

---
+++ src/main/java/harsha/App.java
@@ -0,0 +1,13 @@
+package harsha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Sahiti this is Harsha !" );
+    }
+}
